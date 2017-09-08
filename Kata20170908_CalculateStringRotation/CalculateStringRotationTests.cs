7d7d0ef355365426d5fa7651e954a841c41a9bab
cash <cash@cashwu.com>
2017-09-08 19:04:37 +0800

    init

--- Kata20170908_CalculateStringRotation/CalculateStringRotationTests.cs
+++ Kata20170908_CalculateStringRotation/CalculateStringRotationTests.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Kata20170908_CalculateStringRotation
 {
     [TestClass]
-    public class UnitTest1
+    public class CalculateStringRotationTests
     {
         [TestMethod]
         public void TestMethod1()
