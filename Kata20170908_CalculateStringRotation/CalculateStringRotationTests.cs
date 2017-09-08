2e8036e21d61d60f3f2129deeb54284aebb16291
cash <cash@cashwu.com>
2017-09-08 20:38:21 +0800

    input abc and cab test case

--- Kata20170908_CalculateStringRotation/CalculateStringRotationTests.cs
+++ Kata20170908_CalculateStringRotation/CalculateStringRotationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170908_CalculateStringRotation
@@ -9,7 +10,13 @@ namespace Kata20170908_CalculateStringRotation
         [TestMethod]
         public void input_first_empty_second_empty_should_return_0()
         {
-            CalculateRotationStringShouldBe(0, "", "");
+            CalculateRotationStringShouldBe(0, " ", " ");
+        }
+
+        [TestMethod]
+        public void input_first_abc_second_cba_should_return_1()
+        {
+            CalculateRotationStringShouldBe(1, "abc", "cab");
         }
 
         private static void CalculateRotationStringShouldBe(int expected, string first, string second)
@@ -24,7 +31,13 @@ namespace Kata20170908_CalculateStringRotation
     {
         public int ShiftedDiff(string first, string second)
         {
-            return 0;
+            if (first == second)
+            {
+                return 0;
+            }
+
+            var firstChar = first.Last();
+            return second.IndexOf(firstChar) + 1;
         }
     }
 }
