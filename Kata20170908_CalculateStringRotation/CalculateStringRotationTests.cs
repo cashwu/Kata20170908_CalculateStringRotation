64b5fd099a29e1363d7a5348c409d77487b91b8a
cash <cash@cashwu.com>
2017-09-08 20:28:11 +0800

    input empty and empty test case

--- Kata20170908_CalculateStringRotation/CalculateStringRotationTests.cs
+++ Kata20170908_CalculateStringRotation/CalculateStringRotationTests.cs
@@ -7,8 +7,24 @@ namespace Kata20170908_CalculateStringRotation
     public class CalculateStringRotationTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_first_empty_second_empty_should_return_0()
         {
+            CalculateRotationStringShouldBe(0, "", "");
+        }
+
+        private static void CalculateRotationStringShouldBe(int expected, string first, string second)
+        {
+            var rotation = new CalculateStringRotation();
+            var actual = rotation.ShiftedDiff(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class CalculateStringRotation
+    {
+        public int ShiftedDiff(string first, string second)
+        {
+            return 0;
         }
     }
 }
